41ea216dd5d2f217462b614f1de7d3809ec8d110
Maris Lorencs <lorencs.maris@gmail.com>
2021-05-17 13:06:40 +0300

    Removed Create React App template

---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+/**
+ * App component renders ... .
+ */
+const App: React.FC = () => {
+    return <div className={styles["app"]}>I Am App Component</div>;
+};
+
+export default App;
